09de3e1b8192516aa2dd2846c03100f73fb249ef
Madhurya Dutta <97863234+madhuryadutta@users.noreply.github.com>
2023-12-26 13:53:20 +0000

    login & register function is working

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Container, Col, Row } from "react-bootstrap";
+// import { Switch, Route } from "react-router-dom";
+
+import "./App.css";
+import Register from "./Register";
+import Login from "./Login";
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col xs={12} sm={12} md={6} lg={6}>
+          <Register />
+        </Col>
+        <Col xs={12} sm={12} md={6} lg={6}>
+          <Login />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import axios from "axios";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, setLogin] = useState(false);
+
+  const handleSubmit = (e) => {
+    // set configurations
+    const configuration = {
+      method: "post",
+      url: "https://api-protidhwanidigital.koyeb.app/login",
+      data: {
+        email,
+        password,
+      },
+    };
+    // make the API call
+    axios(configuration)
+      .then((result) => {
+        setLogin(true);
+      })
+      .catch((error) => {
+        error = new Error();
+      });
+
+    // prevent the form from refreshing the whole page
+    e.preventDefault();
+    // make a popup alert showing the "submitted" text
+    alert("Submited");
+  };
+
+  return (
+    <>
+      <h2>Login</h2>
+      <Form onSubmit={(e) => handleSubmit(e)}>
+        {/* email */}
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+          />
+        </Form.Group>
+
+        {/* password */}
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+        </Form.Group>
+
+        {/* submit button */}
+        <Button
+          variant="primary"
+          type="submit"
+          onClick={(e) => handleSubmit(e)}
+        >
+          Submit
+        </Button>
+        {/* display success message */}
+        {login ? (
+          <p className="text-success">You Are Logged in Successfully</p>
+        ) : (
+          <p className="text-danger">You Are Not Logged in</p>
+        )}
+      </Form>
+    </>
+  );
+}
