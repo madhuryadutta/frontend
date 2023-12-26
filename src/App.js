0911ebd3e4cd75cc5e6cd809756e424e9a336a0e
Madhurya Dutta <97863234+madhuryadutta@users.noreply.github.com>
2023-12-26 16:46:12 +0000

    temp patch 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Container, Col, Row } from "react-bootstrap";
-// import Account from "./Account";
+import Account from "./Account";
 import FreeComponent from "./FreeComponent";
 import AuthComponent from "./AuthComponent";
 import ProtectedRoutes from "./ProtectedRoutes";
@@ -19,55 +19,21 @@ function App() {
           </section>
         </Col>
       </Row>
-
-      {/* create routes here */}
-      <Switch>
-        {/* <Route exact path="/" component={Account} /> */}
-        <Route exact path="/free" component={FreeComponent} />
-        <ProtectedRoutes path="/auth" component={AuthComponent} />
-      </Switch>
-    </Container>
-  );
+      <Router>
+        <Fragment>
+          {/* create routes here */}
+          <Routes>
+            <Route exact path="/" component={<Account />} />
+            <Route exact path="/free" component={<FreeComponent />} />
+            <Route exact path='/auth' element={<ProtectedRoutes />}>
+              <Route exact path='/auth' element={<AuthComponent />} />
+            </Route>
+            {/* <ProtectedRoutes path="/auth" component={<AuthComponent />} /> */}
+          </Routes>
+      </Router>
+    </Fragment>
+            </Container >
+            );
 }
 
 export default App;
-
-// import React, { Fragment } from "react";
-// import { Container, Col, Row } from "react-bootstrap";
-// import { Routes, Route } from "react-router-dom";
-
-// import "./App.css";
-// import Register from "./Register";
-// import Login from "./Login";
-// // import Account from "./Account";
-// import FreeComponent from "./FreeComponent";
-// import AuthComponent from "./AuthComponent";
-// import ProtectedRoutes from "./ProtectedRoutes";
-
-// function App() {
-//   return (
-//     <Container>
-//       <Row>
-//         <Col className="text-center">
-//           <h1>React Authentication Tutorial</h1>
-
-//           <section id="navigation">
-//             <a href="/">Home</a>
-//             <a href="/free">Free Component</a>
-//             <a href="/auth">Auth Component</a>
-//           </section>
-//         </Col>
-//       </Row>
-//       {/* create routes here */}
-//       <Routes>
-//         {/* <Rsoute exact path="/" element={<Account />} /> */}
-//         <Route exact path="/free" element={FreeComponent} />
-//         <Fragment>
-//           <ProtectedRoutes path="/auth" element={<AuthComponent />} />
-//         </Fragment>
-//       </Routes>
-//     </Container>
-//   );
-// }
-
-// export default App;
